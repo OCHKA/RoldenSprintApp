c38ac70663c305b3ed758192f6aaf8dcdd9ef77e
weith <turbavladimir@gmail.com>
2019-12-28 00:26:54 +0300

    Simplify app init

--- roldensprint/app.py
+++ roldensprint/app.py
@@ -1,5 +1,4 @@
 from kivy.properties import StringProperty, ListProperty
-from kivy.uix.boxlayout import BoxLayout
 from kivy.app import App
 from kivy.clock import Clock
 
@@ -7,33 +6,24 @@ from .sensor import CoapPeriodSensor
 from .widget.rolling_graph import RollingGraph
 
 
-class RoldenSprintWidget(BoxLayout):
+class RoldenSprintApp(App):
+    refresh_rate = 1 / 30
+
     rpm_history_window = 1000
 
     kv_rpm = StringProperty("00000")
     kv_rpm_history = ListProperty([0] * rpm_history_window)
 
-    def update(self, rpm):
-        self.kv_rpm_history.pop(0)
-        self.kv_rpm_history.append(rpm)
-
-        self.kv_rpm = f"{rpm:010}"
-
-
-class RoldenSprintApp(App):
-    refresh_rate = 1 / 30
-
     sensor = CoapPeriodSensor()
-    widget = None
 
     def build(self):
-        self.widget = RoldenSprintWidget()
         Clock.schedule_interval(self.update, self.refresh_rate)
-
-        return self.widget
 
     def on_start(self):
         self.sensor.start()
 
     def update(self, dt):
-        self.widget.update(self.sensor.rpm)
+        self.kv_rpm_history.pop(0)
+        self.kv_rpm_history.append(self.sensor.rpm)
+
+        self.kv_rpm = f"{self.sensor.rpm:010}"
