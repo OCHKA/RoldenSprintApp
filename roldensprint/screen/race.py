d961146518a7e23fd7f589ea2d90e67cd4ecf573
weith <turbavladimir@gmail.com>
2020-02-25 01:40:45 +0300

    Remove old package

--- roldensprint/screen/race.py
+++
@@ -1,8 +0,0 @@
-from kivy.uix.screenmanager import Screen
-
-from roldensprint.widget.racer import RacerWidget
-from roldensprint.widget.graph import GraphWidget
-
-
-class RaceScreen(Screen):
-    pass
