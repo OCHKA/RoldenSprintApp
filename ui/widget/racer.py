2e6c1323abdad10f16272188b7a8f9ad7c776246
weith <turbavladimir@gmail.com>
2020-02-25 23:11:28 +0300

    [racer_widget] Add distance reset, Improve speed meter

--- ui/widget/racer.py
+++ ui/widget/racer.py
@@ -6,29 +6,40 @@ from pubsub import pub
 
 class RacerWidget(BoxLayout):
     name = StringProperty("nobody")
-    speed = NumericProperty(0)
-    distance = NumericProperty(0)
-    race_distance = NumericProperty(1000)
     index = NumericProperty(0)
+    race_distance = NumericProperty(1000)
 
+    _speed = StringProperty('000 KPH')
+    _distance = NumericProperty(0)
     _progress_text = StringProperty(f"0 / {race_distance.defaultvalue} M")
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+        self._start_distance = None
+
         pub.subscribe(self.on_speed_data, f'racer.{self.index}.speed')
         pub.subscribe(self.on_distance_data, f'racer.{self.index}.distance')
 
+    def reset_position(self):
+        self._start_distance = self.distance
+
     def on_speed_data(self, speed):
-        self.speed = speed * 3.6
+        speed_kph = speed * 3.6
+        self._speed = f"{speed_kph:06.2f} KPH"
 
     def on_distance_data(self, distance):
-        self.distance = distance
+        if self._start_distance is None:
+            self._start_distance = distance
 
-        if distance >= self.race_distance:
+        self._distance = distance - self._start_distance
+        self._update_progress_text()
+
+    def _update_progress_text(self):
+        if self._distance >= self.race_distance:
             self._progress_text = "FINISHED"
         else:
-            self._progress_text = f"{distance:.2f} / {self.race_distance} M"
+            self._progress_text = f"{self._distance:.2f} / {self.race_distance} M"
 
 
 Builder.load_string('''
@@ -58,11 +69,11 @@ Builder.load_string('''
         LabelTemplate:
             text: root.name
         LabelTemplate:
-            text: str(int(root.speed)) + " KPH"
+            text: root._speed
         LabelTemplate:
             text: root._progress_text
     
     ProgressBar:
         max: root.race_distance
-        value: root.distance
+        value: root._distance
 ''')
